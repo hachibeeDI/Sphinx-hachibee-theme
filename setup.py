f7e37c1232c33bd6152c32f7f92d375bc40b26f2
dogura <daiki.ogura@soliton.co.jp>
2013-12-06 18:51:22 +0900

    flip sidebar and document
    and fix some barance

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hachibee-sphinx-theme',
-    version='0.1.1',
+    version='0.1.3',
     author='OGURA_Daiki',
     author_email='8hachibee125@gmail.com',
     url='https://github.com/hachibeeDI/Sphinx-hachibee-theme',
