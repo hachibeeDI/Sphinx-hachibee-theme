e0b524c14d7cfc309e65d0ffed8ac6affa6a2893
dogura <daiki.ogura@soliton.co.jp>
2014-02-05 13:04:35 +0900

    add some desc on setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
+import os
 from setuptools import setup, find_packages
 
+DESCRIPTION = open(
+    os.path.join(os.path.dirname(__file__), 'README.md')).read().strip()
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Web Environment",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Documentation",
+]
 
 setup(
     name='hachibee-sphinx-theme',
@@ -8,9 +19,12 @@ setup(
     author_email='8hachibee125@gmail.com',
     url='https://github.com/hachibeeDI/Sphinx-hachibee-theme',
     description='a simple sphinx theme',
+    long_description=DESCRIPTION,
     packages=find_packages('src'),
     package_dir={'': 'src'},
     license="MIT",
     include_package_data=True,
     entry_points='',
+    keywords=['sphinx', 'sphinx.theme', ],
+    classifiers=classifiers,
 )
