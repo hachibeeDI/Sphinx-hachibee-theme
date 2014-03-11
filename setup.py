f6e2e70d974847e287cb81c21d1f4dfc0092f404
dogura <daiki.ogura@soliton.co.jp>
2014-03-11 12:10:07 +0900

    fix contenttree bug

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ classifiers = [
 
 setup(
     name='hachibee-sphinx-theme',
-    version='0.1.9',
+    version='0.1.9.1',
     author='OGURA_Daiki',
     author_email='8hachibee125@gmail.com',
     url='https://github.com/hachibeeDI/Sphinx-hachibee-theme',
