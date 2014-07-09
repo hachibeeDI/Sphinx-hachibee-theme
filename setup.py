f9899549ab302bc2ef134479cd38c8cb3631fc40
dogura <daiki.ogura@soliton.co.jp>
2014-07-09 11:37:40 +0900

    update to v0.2.2
    define ascii font (Avnir Next)
    define monospace font for pre
    tweak some role's style

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ classifiers = [
 
 setup(
     name='hachibee-sphinx-theme',
-    version='0.2.1',
+    version='0.2.2',
     author='OGURA_Daiki',
     author_email='8hachibee125@gmail.com',
     url='https://github.com/hachibeeDI/Sphinx-hachibee-theme',
